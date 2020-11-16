835fa406e9fc547487f8a6a0d97a4c73a3706596
Pulkit  Tyagi <pulkittyagi@Pulkits-MBP.bbrouter>
2020-11-16 23:54:51 +0530

    Alive2

---
+++ File2.R
@@ -0,0 +1,23 @@
+mtcars 
+mtcars
+#vector----
+x= c(1,2,3,6) # command+ enter
+
+x= c('a','b','me')
+class(x)
+x2= c(1,2,4,5,6)
+
+mean(x2)
+
+sum(x2)
+x3<- 1:100
+x3
+x4<- seq(1,100,3)
+x4
+?seq
+#matrix----
+
+
+
+#dataframe----
+
